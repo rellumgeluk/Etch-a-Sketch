b31962683e70dfcf6cf74e958e7160b8a78cbeab
Nathalia <nathalia96234@gmail.com>
2024-07-04 18:54:10 +0100

    Functional

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('container');
-}
+
+    function createGrid(size) {
+        for(let i=0; i < size * size; i++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            container.appendChild(cell);
+        }
+    }
+
+    function enableDrawing() {
+        const cells= document.querySelectorAll('.cell');
+        cells.forEach(cell => {
+            cell.addEventListener('mouseover', () => {
+                cell.style.backgroundColor = 'pink';
+            });
+        });
+    }
+
+    createGrid(16);
+    enableDrawing();
+});
